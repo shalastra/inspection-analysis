9107f4e5e4e30977cffc189e46be9117ad38087b
Szymon Halastra <szymon.edward.halastra@cern.ch>
2019-06-20 17:12:32 +0200

    Add basic connection

--- inspection-app/src/main/java/io/shalastra/App.java
+++ inspection-app/src/main/java/io/shalastra/App.java
@@ -1,5 +1,7 @@
 package io.shalastra;
 
+import java.sql.*;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -7,5 +9,19 @@ public class App {
 
   public static void main(String[] args) {
     log.info("test");
+
+    try(Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5431/inspections_db", "postgres", "postgres")) {
+      log.info("Java JDBC PostgreSQL Example");
+
+      log.info("Connected to PostgreSQL database!");
+      Statement statement = connection.createStatement();
+      log.info("Reading inspections records...");
+      ResultSet resultSet = statement.executeQuery("SELECT * FROM public.inspections LIMIT 10");
+      while (resultSet.next()) {
+        log.info(resultSet.getString("inspection_id") + " " + resultSet.getString("dba_name"));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 }
